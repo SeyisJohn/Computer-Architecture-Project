9883ae9400da52948ec6f572c0eff0d9fe38b90a
SeyisJohn <22299479+SeyisJohn@users.noreply.github.com>
2020-11-16 13:25:06 -0500

    Continuation of creating ALU operations

--- ALU/Multimedia ALU/src/ALU.vhd
+++ ALU/Multimedia ALU/src/ALU.vhd
@@ -28,16 +28,11 @@ use IEEE.numeric_std.all;
 
 entity ALU is
 	 port(
---		 rs1 : in STD_LOGIC_VECTOR(127 downto 0);
---		 rs2 : in STD_LOGIC_VECTOR(127 downto 0);
---		 rs3 : in STD_LOGIC_VECTOR(127 downto 0);
---		 instruct : in STD_LOGIC_VECTOR(4 downto 0);
---		 output : out STD_LOGIC_VECTOR(127 downto 0)
-		 rs1 : in STD_LOGIC_VECTOR(15 downto 0);
-		 rs2 : in STD_LOGIC_VECTOR(15 downto 0);
-		 rs3 : in STD_LOGIC_VECTOR(15 downto 0);
+		 rs1 : in STD_LOGIC_VECTOR(127 downto 0);
+		 rs2 : in STD_LOGIC_VECTOR(127 downto 0);
+		 rs3 : in STD_LOGIC_VECTOR(127 downto 0);
 		 instruct : in STD_LOGIC_VECTOR(4 downto 0);
-		 output : out STD_LOGIC_VECTOR(31 downto 0)
+		 output : out STD_LOGIC_VECTOR(127 downto 0)
 	     );
 end ALU;
 
@@ -45,120 +40,112 @@ end ALU;
 
 architecture ALU of ALU is
 	
--- This multiply is performed with saturated rounding
--- This means that instead of over/underflow wrapping, the max/min values are used.
--- This is booth's algorithm
-	function signed_int_multiply (
-		multiplicand, multiplicator: SIGNED(15 downto 0)) 
+	function signed_int_multiply_add (
+	multiplicand, multiplicator: SIGNED(15 downto 0);
+	add_value : SIGNED(31 downto 0)) 
 		return STD_LOGIC_VECTOR is 
-		variable neg_multiplicand : SIGNED(15 downto 0); 
-		variable c_bits : STD_LOGIC_VECTOR(1 downto 0);
-		variable product : SIGNED(31 downto 0) := (others => '0');
+		variable value, product : SIGNED(31 downto 0) := (others => '0');
 	begin
 		
-		-- Start with point being zero
-		product(15 downto 0) := SIGNED(multiplicator);
-		neg_multiplicand := (not(multiplicand) + '1');
-		c_bits(0) := '0';
-		c_bits(1) := product(1);
+		product := multiplicand	* multiplicator; 
 		
+		value := product + add_value;
 		
-		if (c_bits = "10") then
-			product(31 downto 16) := product(31 downto 16) + neg_multiplicand;
-			product := shift_right(signed(product), 1);
-			c_bits(0) := c_bits(1);
-			c_bits(1) := product(0);
-		elsif (c_bits = "01") then
-			product(31 downto 16) := product(31 downto 16) + multiplicand;
-			product := shift_right(signed(product), 1);
-			c_bits(0) := c_bits(1);
-			c_bits(1) := product(0);
+		if (product > 0 and add_value > 0 and value < product) then
+			value := X"7FFF_FFFF";
+		elsif (product < 0 and add_value < 0 and value > product) then
+			value := X"8000_0000";
 		else
-			product := shift_right(signed(product), 1);
-			c_bits(0) := c_bits(1);
-			c_bits(1) := product(0);
-		end if;
+			null;
+		end if;	
 		
-		
-		for i in 14 downto 0 loop
-			if (c_bits = "10") then
-				product(31 downto 16) := product(31 downto 16) + neg_multiplicand;
-				product := shift_right(signed(product), 1);
-				c_bits(0) := c_bits(1);
-				c_bits(1) := product(0);
-			elsif (c_bits = "01") then
-				product(31 downto 16) := product(31 downto 16) + multiplicand;
-				product := shift_right(signed(product), 1);
-				c_bits(0) := c_bits(1);
-				c_bits(1) := product(0);
-			else
-				product := shift_right(signed(product), 1);
-				c_bits(0) := c_bits(1);
-				c_bits(1) := product(0);
-			end if;
-		end loop; 
-		
-		return STD_LOGIC_VECTOR(product);	
+		return STD_LOGIC_VECTOR(value);	
 	
-	end signed_int_multiply;
+	end signed_int_multiply_add;	
+	
+	
+	function signed_long_multiply_add (
+	multiplicand, multiplicator: SIGNED(31 downto 0);
+	add_value : SIGNED(63 downto 0)) 
+		return STD_LOGIC_VECTOR is 
+		variable value, product : SIGNED(63 downto 0) := (others => '0');
+	begin
+		
+		product := multiplicand	* multiplicator; 
+		
+		value := product + add_value;
+		
+		if (product > 0 and add_value > 0 and value < product) then
+			value := X"7FFF_FFFF_FFFF_FFFF";
+		elsif (product < 0 and add_value < 0 and value > product) then
+			value := X"8000_0000_0000_0000";
+		else
+			null;
+		end if;	
+		
+		return STD_LOGIC_VECTOR(value);	
+	
+	end signed_long_multiply_add;
 		
 		
 
 begin
 
-	process (instruct, rs1, rs2, rs3)
-	begin
-		
-		if (instruct = X"00") then
-			-- Signed Integer Multiply-Add Low with Saturation
-		elsif (instruct = X"01") then
-			-- Signed Integer Multiply-Add High with Saturation
-		elsif (instruct = X"02") then
-			-- Signed Integer Multiply-Subtract Low with Saturation
-		elsif (instruct = X"03") then
-			-- Signed Integer Multiply-Subtract High with Saturation
-		elsif (instruct = X"04") then
-			-- Signed Long Integer Multiply-Add Low with Saturation
-		elsif (instruct = X"05") then
-			-- Signed Long Integer Multiply-Add High with Saturation
-		elsif (instruct = X"06") then
-			-- Signed Long Integer Multiply-Subtract Low with Saturation
-		elsif (instruct = X"07") then 
-			-- Signed Long Integer Multiply-Subtract High with Saturation
-		elsif (instruct = X"08") then 
-			-- add word unsigned
-		elsif (instruct = X"09") then
-			-- absolute difference of bytes
-		elsif (instruct = X"0A") then
-			-- add halfword unsigned
-		elsif (instruct = X"0B") then
-			-- add halfword saturated 
-		elsif (instruct = X"0C") then
-			output <= rs1 and rs2;
-		elsif (instruct = X"0D") then
-			-- broadcast word 
-		elsif (instruct = X"0E") then
-			-- max signed word
-		elsif (instruct = X"10") then
-			-- min signed word
-		elsif (instruct = X"11") then
-			-- multiply low unsigned
-		elsif (instruct = X"12") then
-			-- multiply low by constant unsigned
-		elsif (instruct = X"13") then
-			output <= rs1 or rs2;
-		elsif (instruct = X"14") then
-			-- count ones in words
-		elsif (instruct = X"15") then
-			-- rotate bits in word
-		elsif (instruct = X"16") then
-			-- subtract from halfword saturated
-		elsif (instruct = X"17") then
-			-- subtract from word unsigned			
-		else
-			--NOP
-		end if;
-			
-	output <= signed_int_multiply(SIGNED(rs1), SIGNED(rs2));
+--	process (instruct, rs1, rs2, rs3)
+--	begin
+--		
+--		if (instruct = X"00") then
+--			-- Signed Integer Multiply-Add Low with Saturation
+--		elsif (instruct = X"01") then
+--			-- Signed Integer Multiply-Add High with Saturation
+--		elsif (instruct = X"02") then
+--			-- Signed Integer Multiply-Subtract Low with Saturation
+--		elsif (instruct = X"03") then
+--			-- Signed Integer Multiply-Subtract High with Saturation
+--		elsif (instruct = X"04") then
+--			-- Signed Long Integer Multiply-Add Low with Saturation
+--		elsif (instruct = X"05") then
+--			-- Signed Long Integer Multiply-Add High with Saturation
+--		elsif (instruct = X"06") then
+--			-- Signed Long Integer Multiply-Subtract Low with Saturation
+--		elsif (instruct = X"07") then 
+--			-- Signed Long Integer Multiply-Subtract High with Saturation
+--		elsif (instruct = X"08") then 
+--			-- add word unsigned
+--		elsif (instruct = X"09") then
+--			-- absolute difference of bytes
+--		elsif (instruct = X"0A") then
+--			-- add halfword unsigned
+--		elsif (instruct = X"0B") then
+--			-- add halfword saturated 
+--		elsif (instruct = X"0C") then
+--			output <= rs1 and rs2;
+--		elsif (instruct = X"0D") then
+--			-- broadcast word 
+--		elsif (instruct = X"0E") then
+--			-- max signed word
+--		elsif (instruct = X"10") then
+--			-- min signed word
+--		elsif (instruct = X"11") then
+--			-- multiply low unsigned
+--		elsif (instruct = X"12") then
+--			-- multiply low by constant unsigned
+--		elsif (instruct = X"13") then
+--			output <= rs1 or rs2;
+--		elsif (instruct = X"14") then
+--			-- count ones in words
+--		elsif (instruct = X"15") then
+--			-- rotate bits in word
+--		elsif (instruct = X"16") then
+--			-- subtract from halfword saturated
+--		elsif (instruct = X"17") then
+--			-- subtract from word unsigned			
+--		else
+--			--NOP
+--		end if;
+--
+--	end process;
 
+	output(31 downto 0) <= signed_int_multiply(SIGNED(rs1(15 downto 0)), SIGNED(rs2(15 downto 0)));
+	
 end ALU;
