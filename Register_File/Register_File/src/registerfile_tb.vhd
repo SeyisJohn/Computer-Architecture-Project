0a376e30d75e6a260ccbc5079f86f656088ff36e
Kevin Mathew <kmathew231@gmail.com>
2020-11-30 03:04:01 -0500

    Finished testbench and documentation

--- Register_File/Register_File/src/registerfile_tb.vhd
+++ Register_File/Register_File/src/registerfile_tb.vhd
@@ -25,70 +25,65 @@ use ieee.numeric_std.all;
 library work;
 use work.all;
 
-entity registerfile_tb is end reigsterfile_tb;
+entity registerfile_tb is end registerfile_tb;
 	
 architecture registerfile_tb of registerfile_tb is 
--- Stimulated Signals
-		signal writeData 		: STD_LOGIC_VECTOR(127 downto 0);   	-- rd    
-		signal writeRegSel		: STD_LOGIC_VECTOR(4 downto 0);	   	-- rd
-		signal writeEnable 		: STD_LOGIC; 			 			-- write enable	
-		signal input 			: STD_LOGIC_VECTOR(24 downto 0);		-- 25 bit instruction to be decode
-		signal clk 				: STD_LOGIC := 0 					    	-- clock  			   
+--Stimulated Signals
+signal		 writeData 				: STD_LOGIC_VECTOR(127 downto 0);  	-- rd   
+signal		 writeRegSel			: STD_LOGIC_VECTOR(4 downto 0);	   	-- rd
+signal		 writeEnable 			: STD_LOGIC; 			 			-- write enable	
+signal		 input 					: STD_LOGIC_VECTOR(24 downto 0);	-- 25 bit instruction to be decoded
+--Output Signals		 
+signal		 ALU_Ctrl				: STD_LOGIC_VECTOR(4 downto 0);     -- ALU control signal
+signal		 vRSel1                 : STD_LOGIC;  						--Specify if the rs1 output is a valid register for forwarding
+signal		 vRSel2                 : STD_LOGIC;  						--Specify if the rs2 output is a valid register for forwarding
+signal		 vRSel3                 : STD_LOGIC;	  						--Specify if the rs3 output is a valid register for forwarding
+signal		 RSel1					: STD_LOGIC_VECTOR(4 downto 0);	   	--  output rs1
+signal		 RSel2 					: STD_LOGIC_VECTOR(4 downto 0);	   	--  output rs2
+signal		 RSel3 					: STD_LOGIC_VECTOR(4 downto 0);	   	--  output rs3
+signal		 RSelD 					: STD_LOGIC_VECTOR(4 downto 0);    	-- output rd
+signal		 rs1  					: STD_LOGIC_VECTOR(127 downto 0);	-- rs1
+signal		 rs2  					: STD_LOGIC_VECTOR(127 downto 0);	-- rs2
+signal		 rs3  					: STD_LOGIC_VECTOR(127 downto 0);	-- rs3
 
---Output Signals 
-		signal rs1  			: STD_LOGIC_VECTOR(127 downto 0) := (others => '0');	-- rs1 data
-		signal rs2  			: STD_LOGIC_VECTOR(127 downto 0) := (others => '0');	-- rs2 data
-		signal rs3  			: STD_LOGIC_VECTOR(127 downto 0) := (others => '0');	-- rs3 data	 
-		signal RSel1			: STD_LOGIC_VECTOR(4 downto 0);	   	--  output rs1
-		signal RSel2 			: STD_LOGIC_VECTOR(4 downto 0);	   	--  output rs2
-		signal RSel3 			: STD_LOGIC_VECTOR(4 downto 0);	   	--  output rs3
-		signal RselD 			: STD_LOGIC_VECTOR(4 downto 0);   	-- output rd	 
-		signal ALU_Ctrl			: STD_LOGIC_VECTOR(4 downto 0);					 
-		
 begin 
 	UUT: entity register_file port map(
 		writeData => writeData,
 		writeRegSel => writeRegSel,
 		writeEnable => writeEnable,
 		input => input,			
-		clk => clk,			 			   
-		rs1  =>	rs1,		
-		rs2  =>	rs2,	
-		rs3  =>	rs3,		 
+		ALU_Ctrl =>	ALU_Ctrl,
+		vRSel1 => vRSel1,
+		vRSel2 => vRSel2,
+		vRSel3 => vRSel3,
 		RSel1 => RSel1,			
 		RSel2 => RSel2,			
-		RSel3 => RSel3,		
-		RselD => RselD,			
-		ALU_Ctrl =>	ALU_Ctrl );			 
+		RSel3 => RSel3,		 			   
+		RselD => RselD,
+		rs1  =>	rs1,		
+		rs2  =>	rs2,	
+		rs3  =>	rs3		 
+	);			 
 		
 		
-		R3_instruction : process 
-		begin 
-			
-			
-			
-			
-			
-			
-			
-			
-			
-				wait;
-		end process;
-		
-		R4_instruction : process
-		begin 
-			
-			
-			
+		Instructions : process 
+		begin  
+			input <= "1100000001010110111011110";
+			wait for 600ns;
+			input <= "1000010110010100010100010";
+			wait for 600ns;
+			input <= "0011000000000000111001110";
+		 	wait for 600ns;
 			wait;
 		end process;
-		
+	
 		writeRD : process
-		begin
-			
-			
-			
+		begin 
+			writeEnable <= '0';
+			wait for 1200ns;
+			writeEnable <= '1';
+			writeData <= x"bbbbcbbbbbbbbbbbbbbbbbbbbbbbbbab";
+			writeRegSel <= "00110";
 			wait;
 		end process;
 
